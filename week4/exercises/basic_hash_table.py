995998d7a2b7291705e45a28f7961cd1cb0c1e88
Ismail K <ismailkefokeris@gmail.com>
2020-10-22 12:34:20 +0100

    Completed Basic HashTables with separate Chaining...

--- week4/exercises/basic_hash_table.py
+++ week4/exercises/basic_hash_table.py
@@ -1,1 +1,51 @@
 
+
+class hashtable():
+
+    def __init__(self):
+        self.table = [None] * 10 
+        
+    def put(self, key, value):
+        hashcode = 0
+        count = 0
+
+        for char in key:
+            hashcode += ord(char) * (31**count)
+            count += 1
+
+        pos = hashcode % 10
+        if self.table[pos] is None:
+            self.table[pos] = []
+
+        self.table[pos].append((key,value))
+
+    def get(self, key):
+        hashcode = 0
+        count = 0
+        count2 = 0
+
+        for char in key:
+            hashcode += ord(char) * (31**count)
+            count += 1
+
+        pos = hashcode % 10
+
+        if self.table[pos] is None:
+            return ("ERROR Nothing stored with the key: {}".format(key))
+        else:
+            while count2 != len(self.table[pos]):
+
+                if count2 == self.table[pos[count2, 0]]:
+                    print(self.table[pos[count2,0]])
+                    print(self.table[pos[count2,1]])
+
+                count2 += 1        
+
+    def __str__(self):
+        return self.table.__str__()
+
+table = hashtable()
+
+table.put("cat", "A Furry Animal which goes boomies")
+
+table.put("act", "skawies animools")
