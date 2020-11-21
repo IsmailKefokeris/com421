2d67285211ba47de9da0bb9200f3c728f7d2a241
Ismail K <ismailkefokeris@gmail.com>
2020-11-21 23:16:41 +0000

    Restarting AE1 due to harddrive errors

---
+++ assignments/ae1/ae1.py
@@ -0,0 +1,20 @@
+
+
+class poi():
+    def __init__(self):
+        self.poi = ()
+        
+    
+    def add(self, name, type, description):
+        pass
+
+    def search(self, name):
+        pass
+
+    def display(self):
+        pass
+
+
+
+def run():
+    pass
